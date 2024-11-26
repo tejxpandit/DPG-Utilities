d9bb7123058252ca16b36a65a04d0c88c71f52b1
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-11-25 22:34:22 -0600

    Image Import Class
    Created a base class for importing and converting images to DPG from other libraries and formats.

---
+++ ImageImport.py
@@ -0,0 +1,10 @@
+# File      : Image Importer Class for DearPyGUI in Python
+# Author    : Tej Pandit
+# Date      : Oct 2024
+
+import dearpygui.dearpygui as dpg
+import numpy as np
+
+class ImageImport:
+    def __init__(self):
+        self.image_type = "file"
