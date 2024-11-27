d63e0fb17410bc6710538e1b569cbe8abde0b2ce
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-11-26 23:37:21 -0600

    Image Type Selection
    A function to set the image type.

--- ImageImport.py
+++ ImageImport.py
@@ -8,3 +8,10 @@ import numpy as np
 class ImageImport:
     def __init__(self):
         self.image_type = "file"
+        self.img_w = None
+        self.img_h = None
+        self.img_c = None
+        self.img_d = None
+
+    def setImageType(self, image_type):
+        self.image_type = image_type
